6d9ae13544b5cd37d6b0d8e0da8e4216026ecea2
humtech <udalov0078@mail.ru>
2025-12-28 01:25:10 +0300

    add cer wer metrics with lib editdistance

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,13 +1,25 @@
-# Based on seminar materials
-
-# Don't forget to support cases when target_text == ''
+import editdistance
 
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    target_text = target_text or ""
+    predicted_text = predicted_text or ""
+
+    if target_text == "":
+        return 0.0 if predicted_text == "" else 1.0
+
+    dist = editdistance.eval(target_text, predicted_text)
+    return dist / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO
-    pass
+    target_text = target_text or ""
+    predicted_text = predicted_text or ""
+
+    target_words = target_text.split()
+    predicted_words = predicted_text.split()
+    if len(target_words) == 0:
+        return 0.0 if len(predicted_words) == 0 else 1.0
+
+    dist = editdistance.eval(target_words, predicted_words)
+    return dist / len(target_words)

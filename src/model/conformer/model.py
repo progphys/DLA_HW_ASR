7905e5dd4ffab6d1e42b30a891a2863e67e17e2f
humtech <udalov0078@mail.ru>
2025-12-29 17:06:55 +0300

    collect all parts of ConformerCTCModel

--- src/model/conformer/model.py
+++ src/model/conformer/model.py
@@ -1,21 +1,56 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from conformer_blocks import ConformerBlocks
+from subsampling import Conv2dSubsampling4
+
+
+def lengths_to_padding_mask(lengths: torch.Tensor, max_len: int) -> torch.Tensor:
+    return torch.arange(max_len, device=lengths.device).unsqueeze(
+        0
+    ) >= lengths.unsqueeze(1)
 
 
 class ConformerCTCModel(nn.Module):
     def __init__(
         self,
-        n_tokens: int,
-        n_mels: int = 128,
-        d_model: int = 144,
-        **kwargs,
+        n_tokens,
+        n_mels=128,
+        in_features=144,
+        n_heads=4,
+        fc_features=576,
+        num_layers=4,
+        kernel_size=31,
+        dropout=0.1,
     ):
         super().__init__()
-        self.n_tokens = n_tokens
-        self.n_mels = n_mels
-        self.d_model = d_model
-        self.proj = nn.Linear(d_model, n_tokens)
+        self.subsampling = Conv2dSubsampling4(
+            n_mels=n_mels, in_features=in_features, dropout=dropout
+        )
 
-    def forward(self, spectrogram, spectrogram_length, **batch):
-        pass
+        self.encoder = ConformerBlocks(
+            num_layers=num_layers,
+            in_features=in_features,
+            n_heads=n_heads,
+            fc_features=fc_features,
+            kernel_size=kernel_size,
+            dropout=dropout,
+        )
+
+        self.proj = nn.Linear(in_features, n_tokens)
+
+    def forward(
+        self, spectrogram: torch.Tensor, spectrogram_length: torch.Tensor, **batch
+    ):
+        x, out_len = self.subsampling(spectrogram, spectrogram_length)
+
+        pad_mask = lengths_to_padding_mask(out_len, max_len=x.size(1))
+
+        x = self.encoder(x, key_padding_mask=pad_mask)
+
+        log_probs = F.log_softmax(self.proj(x), dim=-1)
+
+        return {
+            "log_probs": log_probs,
+            "log_probs_length": out_len,
+        }
